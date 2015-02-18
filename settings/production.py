eeec95d950d3ed8dbb6214cf39dda7e2df28aa4e
David Barnett <david@barnett.net.nz>
2015-02-19 11:42:48 +1300

    init commit

---
+++ settings/production.py
@@ -0,0 +1,19 @@
+import os
+
+from .defaults import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+SECRET_KEY = os.environ['DJANGO_FETCH_SECRET_KEY']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DJANGO_FETCH_POSTGRES_DB_NAME'],
+        'USER': os.environ['DJANGO_FETCH_POSTGRES_USER'],
+        'PASSWORD': os.environ['DJANGO_FETCH_POSTGRES_PASS'],
+        'HOST': os.environ['DJANGO_FETCH_POSTGRES_HOST'],
+        'PORT': os.environ['DJANGO_FETCH_POSTGRES_PORT'],
+    }
+}
