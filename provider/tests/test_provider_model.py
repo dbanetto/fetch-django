22305b4b118eb8aef6367d9ff01a229187e46d1a
David Barnett <david@barnett.net.nz>
2015-03-13 11:42:46 +1300

    Fixed tests

--- provider/tests/test_provider_model.py
+++ provider/tests/test_provider_model.py
@@ -5,8 +5,4 @@ from provider.models import Provider
 
 class ProviderMethodTests(TestCase):
     fixtures = ['test_provider.json']
-
-    def test_get_avianle_options(self):
-        prov = Provider.objects.filter(pk=1)[0]
-        self.assertEqual(['quality','options'],
-                         prov.get_available_options())
+    pass
