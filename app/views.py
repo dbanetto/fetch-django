c5253d2ec15e64da9c54ed35993e0bc7542c8d82
David Barnett <david@barnett.net.nz>
2015-04-17 10:44:00 +1200

    'This Week' in index
    * added "this week" in app:index
    * refactored series_card
    * added `with`s to template include's

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 
 from django.shortcuts import render, get_object_or_404
@@ -7,6 +7,15 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 
+from series.models import Series
 
 def index(request):
-    return render(request, 'app/index.html')
+    series = []
+    for s in Series.objects.all():
+        next = s.next_release()
+        if next is not None and next < datetime.now() + timedelta(days=7):
+            series.append(s)
+    series.sort(key=lambda s: s.next_release() if type(s.next_release()) is datetime else datetime.max)
+
+    return render(request, 'app/index.html',
+                  {'series_week': series})
