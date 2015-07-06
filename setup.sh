f20885171169f5759dda2fef60da5cc160cee855
David Barnett <david@barnett.net.nz>
2015-07-06 15:10:27 +1200

    tweaked setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+[ ! -n "$(command -v pip)" ] && echo "Requires pip to install" && exit
+[ ! -n "$(command -v npm)" ] && echo "Requires npm to install" && exit
+[ ! -n "$(command -v bower)" ] && echo "Requires bower to install" && exit
+
 # python
 pip install -r requirements/common.txt
 
-# bower - frontent
+# bower - frontend
 python manage.py bower install
 
 # npm - precompilers
-cd components
-npm install
-cd ..
+(cd components ; npm install)
 
 # django
 python manage.py migrate
