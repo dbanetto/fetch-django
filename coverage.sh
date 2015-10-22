19d0b25afba41bf212832ec5cbdeaa437f958cb0
David Barnett <david@barnett.net.nz>
2015-10-22 19:36:48 +1300

    Fix not reporting test failure

--- coverage.sh
+++ coverage.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 coverage run --omit='settings/*','*/migrations/*','*/tests/*','components/*','*/wsgi.py' --source='.' manage.py test --settings=settings.test
-coverage html
+# coverage html
