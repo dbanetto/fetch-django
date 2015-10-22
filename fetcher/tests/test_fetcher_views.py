e5ab4db887f921dfdb6aba41ed9ec7c8f8a06f33
David Barnett <david@barnett.net.nz>
2015-10-22 21:58:12 +1300

    Add simple fetcher view tests

--- fetcher/tests/test_fetcher_views.py
+++ fetcher/tests/test_fetcher_views.py
@@ -1,3 +1,4 @@
+import json
 from django.test import TestCase, Client
 
 
@@ -16,3 +17,93 @@ class TestFetcherViews(TestCase):
                 expect_templates.remove(t.name)
 
         self.assertEquals(len(expect_templates), 0)
+
+    def test_status_html_redirect(self):
+        c = Client()
+
+        res = c.get('/fetcher/status/')
+
+        self.assertEquals(res.status_code, 302)
+
+        self.assertEquals(len(res.templates), 0)
+        self.assertTrue(res.url.endswith("/fetcher/"))
+
+    def test_force_fetch_html_redirect(self):
+        c = Client()
+
+        res = c.get('/fetcher/force/fetch/')
+
+        self.assertEquals(res.status_code, 302)
+
+        self.assertEquals(len(res.templates), 0)
+        self.assertTrue(res.url.endswith("/fetcher/"))
+
+    def test_force_sort_html_redirect(self):
+        c = Client()
+
+        res = c.get('/fetcher/force/sort/')
+
+        self.assertEquals(res.status_code, 302)
+
+        self.assertEquals(len(res.templates), 0)
+        self.assertTrue(res.url.endswith("/fetcher/"))
+
+    def test_status_json_redirect(self):
+        c = Client()
+
+        res = c.post('/fetcher/status/', CONTENT_TYPE='application/json')
+
+        self.assertEquals(res.status_code, 302)
+
+        self.assertEquals(len(res.templates), 0)
+        self.assertTrue(res.url.endswith("/fetcher/"))
+
+    def test_force_fetch_json_redirect(self):
+        c = Client(CONTENT_TYPE='application/json')
+
+        res = c.get('/fetcher/force/fetch/')
+
+        self.assertEquals(res.status_code, 302)
+
+        self.assertEquals(len(res.templates), 0)
+        self.assertTrue(res.url.endswith("/fetcher/"))
+
+    def test_force_sort_json_redirect(self):
+        c = Client(CONTENT_TYPE='application/json')
+
+        res = c.get('/fetcher/force/fetch/')
+
+        self.assertEquals(res.status_code, 302)
+
+        self.assertEquals(len(res.templates), 0)
+        self.assertTrue(res.url.endswith("/fetcher/"))
+
+    def test_status_json(self):
+        c = Client(CONTENT_TYPE='application/json')
+
+        res = c.get('/fetcher/status/')
+
+        self.assertEquals(res.status_code, 200)
+
+        self.assertEquals(len(res.templates), 0)
+        json.loads(res.content.decode(res.charset))
+
+    def test_force_fetch_post_json(self):
+        c = Client()
+
+        res = c.post('/fetcher/force/fetch/', CONTENT_TYPE='application/json')
+
+        self.assertEquals(res.status_code, 200)
+
+        self.assertEquals(len(res.templates), 0)
+        json.loads(res.content.decode(res.charset))
+
+    def test_force_sort_post_json(self):
+        c = Client(CONTENT_TYPE='application/json')
+
+        res = c.post('/fetcher/force/sort/', CONTENT_TYPE='application/json')
+
+        self.assertEquals(res.status_code, 200)
+
+        self.assertEquals(len(res.templates), 0)
+        json.loads(res.content.decode(res.charset))
