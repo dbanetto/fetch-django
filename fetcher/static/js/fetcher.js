e291c685f7b7182cbff54fdb04956890efc90519
David Barnett <david@barnett.net.nz>
2015-12-27 10:04:10 +1300

    fetcher status is checked over time
    force sort & fetch button disable when unavailable

--- fetcher/static/js/fetcher.js
+++ fetcher/static/js/fetcher.js
@@ -7,15 +7,41 @@ function force(url, from) {
 
   var backup_inner = from.html();
   $.ajax({
-      type: "POST",
-      contentType: "application/json",
-      url: url,
-      beforeSend: function() {
-        from.html("<img src=\"/static/img/activity.gif\"/>");
-      },
-      complete: function() {
-        from.html(backup_inner);
-        from.removeAttr('disabled');
-      }
+    type: "POST",
+    contentType: "application/json",
+    url: url,
+    beforeSend: function() {
+      from.html("<img src=\"/static/img/activity.gif\"/>");
+    },
+    complete: function() {
+      from.html(backup_inner);
+      from.removeAttr('disabled');
+    }
   });
+}
+
+function status(url) {
+  $.ajax({
+    contentType: "application/json",
+    url: url,
+    success: function(data) {
+      if (data['running']) {
+        $('#fetcherd-status').text("Is running");
+        if (data['fetch_lock']) {
+          $('#force-fetch').prop("disabled", true);
+        } else {
+          $('#force-fetch').prop("disabled", false);
+        }
+        if (data['sort_lock']) {
+          $('#force-sort').prop("disabled", true);
+        } else {
+          $('#force-sort').prop("disabled", false);
+        }
+      } else {
+        $('#fetcherd-status').text("Is down");
+        $('#force-fetch').prop("disabled", true);
+        $('#force-sort').prop("disabled", true);
+      }
+    }
+  })
 }
--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -n $1 ]; then
+    $1=""
+fi
+
 [ ! -n "$(command -v pip)" ] && echo "Requires pip to install" && exit
 [ ! -n "$(command -v npm)" ] && echo "Requires npm to install" && exit
 [ ! -n "$(command -v bower)" ] && echo "Missing bower. Installing it" && sudo npm install -g bower
@@ -8,10 +12,10 @@
 pip install -r requirements/common.txt
 
 # bower - frontend
-python manage.py bower install
+python manage.py bower install $1
 
 # npm - precompilers
 (cd components ; npm install)
 
 # django
-python manage.py migrate
+python manage.py migrate $1
