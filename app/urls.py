ddd33b0ab9afbcbf5ba33b299435d43579f816a8
David Barnett <david@barnett.net.nz>
2015-12-27 21:46:58 +1300

    Fix non-root url working

--- app/urls.py
+++ app/urls.py
@@ -5,15 +5,12 @@ from django.conf import settings
 
 from app import views
 
-base = settings.URL_ROOT
+base = r'^%s' % settings.URL_ROOT
 urlpatterns = [
-    url(r'^admin/',
-        include(admin.site.urls), prefix=base),
-    url(r'^provider/',
-        include('provider.urls', namespace='provider'), prefix=base),
-    url(r'^series/',
-        include('series.urls', namespace='series'), prefix=base),
-    url(r'^fetcher/', include('fetcher.urls', namespace="fetcher"), prefix=base),
+    url(base + r'admin/', include(admin.site.urls)),
+    url(base + r'provider/', include('provider.urls', namespace='provider')),
+    url(base + r'series/', include('series.urls', namespace='series')),
+    url(base + r'fetcher/', include('fetcher.urls', namespace="fetcher")),
 
-    url(base + r'$', views.index, name="index", prefix=base),
+    url(base + r'$', views.index, name="index"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
