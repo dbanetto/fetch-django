2623a3388b0a2881bc4adfbeb06c83acbc22d2f6
David Barnett <david@barnett.net.nz>
2016-01-19 22:47:44 +1300

    Improved defaults for media type & base provider

--- code/provider/models/base_provider.py
+++ code/provider/models/base_provider.py
@@ -5,6 +5,16 @@ from jsonfield import JSONField
 
 from app.validators import json_schema_validator
 
+_default='''{
+    "properties":
+    {
+        "id": {
+            "title": "ID",
+            "type": "integer"
+            }
+    },
+    "required": ["id"]
+}'''
 
 class BaseProvider(models.Model):
     """
@@ -15,10 +25,11 @@ class BaseProvider(models.Model):
     name = models.CharField(max_length=160,
                             verbose_name="Base Provider's name")
 
-    available_options = JSONField(default='{"properties": {"id": {"title": "id", "type": "integer", "required": false}}}',
-                                  help_text="A JSON Schema of options that"
+    available_options = JSONField(help_text="A JSON Schema of options that"
                                   " the base provider allows",
-                                  validators=[json_schema_validator])
+                                  validators=[json_schema_validator],
+                                  default=_default)
+
 
     def available_options_json(self):
         return json.dumps(self.available_options)
--- code/series/models/media_types.py
+++ code/series/models/media_types.py
@@ -5,6 +5,16 @@ from django.db import models
 from app.validators import json_schema_validator
 from jsonfield import JSONField
 
+_default='''{
+    "properties":
+    {
+        "id": {
+            "title": "ID",
+            "type": "integer"
+            }
+    },
+    "required": ["id"]
+}'''
 
 class MediaType(models.Model):
     """
@@ -13,10 +23,10 @@ class MediaType(models.Model):
     """
     name = models.CharField(max_length=80,
                             help_text="Name of the media type")
-    available_options = JSONField(default='{"properties": {"id": {"title": "id", "type": "integer", "required": false}}}',
-                                  help_text="A JSON schema of options that"
+    available_options = JSONField(help_text="A JSON schema of options that"
                                   " the media type allows",
-                                  validators=[json_schema_validator])
+                                  validators=[json_schema_validator],
+                                  default=_default)
 
     def __str__(self):
         return self.name
