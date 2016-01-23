a5d8b6820d7684ebf892c321ef79fa4d7688c27c
David Barnett <david@barnett.net.nz>
2016-01-24 11:06:44 +1300

    Docker production setup

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# prevent postgres race
+nc -z db 5432
+n=$?
+while [ $n -ne 0 ]; do
+    sleep 1
+    nc -z db 5432
+    n=$?
+done
+
 # Collect static files
 echo "Collect static files"
 python /code/manage.py collectstatic --noinput
@@ -10,4 +19,9 @@ python /code/manage.py migrate
 
 # Start server
 echo "Starting server"
-python /code/manage.py runserver 0.0.0.0:8000
+if [ "$DJANGO_SETTINGS_MODULE" = "settings.production"]; then
+    # todo: change to uwsgi
+    python /code/manage.py runserver 0.0.0.0:8000
+else
+    python /code/manage.py runserver 0.0.0.0:8000
+fi
