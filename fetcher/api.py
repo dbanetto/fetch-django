4cf16b4475c29e8e15865c361de2c3b8bbec7131
David Barnett <david@barnett.net.nz>
2016-05-07 16:32:35 +1200

    add error logging in fetcher api

--- fetcher/api.py
+++ fetcher/api.py
@@ -2,19 +2,21 @@ from django.conf import settings
 
 import requests
 import json
+import logging
+
+logger = logging.getLogger('fetch.api')
+logger.debug("Fetcher url: {}".format(settings.FETCHER_URL))
 
 
 def status():
+    result = {success: False}
     try:
         re = requests.get(settings.FETCHER_URL + '/status/')
+        logger.debug(re.content)
         result = json.loads(re.content.decode('UTF-8'))
         result['success'] = True
     except Exception as e:
-        result = {
-            'success': False,
-            'running': False,
-            'error': '{}'.format(str(e))
-        }
+        logger.error('status: {}'.format(str(e)))
     return result
 
 
@@ -23,6 +25,7 @@ def force_fetch():
         re = requests.post(settings.FETCHER_URL + '/force/fetch/')
         result = json.loads(re.content.decode('UTF-8'))
     except Exception as e:
+        logger.error('force_fetch: {}'.format(str(e)))
         result = {
             'success': False,
             'error': '{}'.format(str(e))
@@ -35,6 +38,7 @@ def force_sort():
         re = requests.post(settings.FETCHER_URL + '/force/sort/')
         result = json.loads(re.content.decode('UTF-8'))
     except Exception as e:
+        logger.error('force_sort: {}'.format(str(e)))
         result = {
             'success': False,
             'error': '{}'.format(str(e))
