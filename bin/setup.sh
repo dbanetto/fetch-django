f6004d429f52c2cc46024713f871df1a1fce1624
David Barnett <david@barnett.net.nz>
2018-01-26 21:21:50 +1300

    Disable bower check

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,7 +6,7 @@ fi
 
 [ ! -n "$(command -v pip)" ] && echo "Requires pip to install" && exit
 [ ! -n "$(command -v npm)" ] && echo "Requires npm to install" && exit
-[ ! -n "$(command -v bower)" ] && echo "Missing bower. Installing it" && exit
+# [ ! -n "$(command -v bower)" ] && echo "Missing bower. Installing it" && exit
 
 # python
 pip install -r requirements/common.txt
